ecd09dfd6f5e0e754153c18848c39421d5795c16
Matej Marusak <marusak.matej@gmail.com>
2016-12-02 10:13:25 +0100

    Insert into splitter only if enough space
    Fixes #6.
    
    Signed-off-by: Matej Marusak <marusak.matej@gmail.com>

--- bakesim.cc
+++ bakesim.cc
@@ -8,22 +8,25 @@
 #include <iostream>
 #include <ctime>
 
-#define MINIMALNE_MIESANIE      60*30
-#define MAXIMALNE_MIESANIE      60*35
-#define PRIPRAVA_MIXU           60*3
-#define OD_MIXERU_K_MIXERU      60*2
-#define DOBA_VYBERANIA_CESTA    60*5
-#define POCET_CHLEBOV_Z_CESTA_M 88
-#define POCET_CHLEBOV_Z_CESTA_H 91
-#define SIMULACIA               60*60*12
-#define DOBA_DELENIA            4
-#define DOBA_ROLOVANIA          8
-#define MINIMALNE_PREDKYSNUTIE  60*10
-#define MAXIMALNE_PREDKYSNUTIE  60*12
-#define MINIMALNE_KYSNUTIE      60*20
-#define MAXIMALNE_KYSNUTIE      60*25
-#define DOBA_PECENIA            60*35
-#define DOBA_CHLADENIE          60*60*3
+#define MINIMALNE_MIESANIE          60*4
+#define MAXIMALNE_MIESANIE          60*6
+#define PRIPRAVA_MIXU_MIN           60*2
+#define PRIPRAVA_MIXU_MAX           60*3
+#define OD_MIXERU_K_MIXERU          60*2
+#define MIN_OD_MIXERU_K_MIXERU      60*2
+#define MINIMALNE_VYBERANIE_CESTA   60*4
+#define MAXIMALNE_VYBERANIE_CESTA   60*6
+#define POCET_CHLEBOV_Z_CESTA_M     88
+#define POCET_CHLEBOV_Z_CESTA_H     91
+#define SIMULACIA                   60*60*12
+#define DOBA_DELENIA                4
+#define DOBA_ROLOVANIA              8
+#define MINIMALNE_PREDKYSNUTIE      60*10
+#define MAXIMALNE_PREDKYSNUTIE      60*12
+#define MINIMALNE_KYSNUTIE          60*20
+#define MAXIMALNE_KYSNUTIE          60*25
+#define DOBA_PECENIA                60*35
+#define DOBA_CHLADENIE              60*60*3
 
 Store  Mixer("Mixer", 4);
 Store  Rolovacka("Rolovacka", 3);
@@ -34,6 +37,7 @@ Store  Pec("Pec", 600);
 Store  Chladenie("Chladenie", 1500);
 
 Histogram ChliebCas("Doba pecenia chleba", 3800, 60, 10);
+Histogram CestoCas("Doba pripravy cesta", 600, 60, 10);
 
 int dobre_chleby = 0;
 int zle_chleby = 0;
@@ -82,7 +86,7 @@ class Chlieb : public Process {
  */
 class Delicka : public Event {
     //Maximalna volna kapacita
-    int free_capacity = 250;
+    int free_capacity = 1250;
     //Prave bochnikov vo zasobniku cesta
     int in = 0;
     //Prebieha delenie
@@ -121,7 +125,7 @@ class Delicka : public Event {
 Facility Miesac("Chlap co miesa");
 
 /* Pocet ciest, ktore chceme vymiesat*/
-int maximum_ciest = 18;
+int maximum_ciest = 30;
 
 Delicka *d = new Delicka();//TODO cleanup
 
@@ -131,21 +135,32 @@ Delicka *d = new Delicka();//TODO cleanup
  */
 class NoveMiesanie : public Process {
     void Behavior() {
+        int my_id = maximum_ciest;
         Enter(Mixer);//Zober volny mixer
         Seize(Miesac);//Zaber miesaca
         maximum_ciest--;
-        Wait(Exponential(PRIPRAVA_MIXU));//Naloz suroviny a zapni mixer
+        double zaciatok = Time;
+        Wait(Uniform(PRIPRAVA_MIXU_MIN, PRIPRAVA_MIXU_MAX));//Naloz suroviny a zapni mixer
         Release(Miesac);//Pocas miesania nie je pracovnik potrebny
         if (maximum_ciest > 0){
             //Ak este treba, pracovnik prechadza k dalsiemu mixeru
-            (new NoveMiesanie)->Activate(Time+Exponential(OD_MIXERU_K_MIXERU));
+            (new NoveMiesanie)->Activate(Time+MIN_OD_MIXERU_K_MIXERU + Exponential(OD_MIXERU_K_MIXERU));
         }
-        Wait(Exponential(DOBA_VYBERANIA_CESTA));//Vymiesanie cesta
+        Wait(Uniform(MINIMALNE_MIESANIE, MAXIMALNE_MIESANIE));//Vymiesanie cesta
+        //Prednostne vyberie cesto pred pripravou noveho
         Seize(Miesac, HIGHEST_PRIORITY);
+        Wait(Uniform(MINIMALNE_VYBERANIE_CESTA, MAXIMALNE_VYBERANIE_CESTA));//Vylozenie cesta
+        int vymiesane = Uniform(POCET_CHLEBOV_Z_CESTA_M, POCET_CHLEBOV_Z_CESTA_H);
+        // Pokial nie je dost miesta, cakaj
+        while ( d->Free_capacity() < vymiesane){
+            Wait(15);
+        }
         //Vylozenie do delicky
-        d->Insert_new(Uniform(POCET_CHLEBOV_Z_CESTA_M, POCET_CHLEBOV_Z_CESTA_H));
+        d->Insert_new(vymiesane);
         Leave(Mixer);
         Release(Miesac);
+        CestoCas(Time - zaciatok);
+        std::cout<<"Cesto no. "<<my_id<<" time "<<Time-zaciatok<<std::endl;
     }
 };
 
@@ -165,5 +180,6 @@ int main() {
     Pec.Output();
     Chladenie.Output();
     ChliebCas.Output();
+    CestoCas.Output();
     return 0;
 }

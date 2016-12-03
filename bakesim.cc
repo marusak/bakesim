62b7240fc284763cc7817c38afd60797577d2864
Matej Marusak <marusak.matej@gmail.com>
2016-12-03 09:49:56 +0100

    Proof takes twice as long as first proof
    Signed-off-by: Matej Marusak <marusak.matej@gmail.com>

--- bakesim.cc
+++ bakesim.cc
@@ -21,7 +21,7 @@ Facility Miesac("Chlap co miesa");
 Store  Mixer("Mixer", 4);
 Store  Rolovacka("Rolovacka", 3);
 Store  PredKysnutie("PredKysnutie", 180);
-Store  Kysnutie("Kysnutie", 450);
+Store  Kysnutie("Kysnutie", 360);
 Store  Osatka("Osatka", 1000);
 Store  Pec("Pec", 600);
 Store  Chladenie("Chladenie", 1500);
